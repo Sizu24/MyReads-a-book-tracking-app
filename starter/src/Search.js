747fd7058258902796140acfabc381ca31bc24fb
Shahetul Chisty <sizu1124@gmail.com>
2023-06-19 10:10:53 -0700

    Add components for main paage

---
+++ starter/src/Search.js
@@ -0,0 +1,25 @@
+function Search({show, closePage}) {
+    return(
+        <div className="search-books">
+          <div className="search-books-bar">
+            <a
+              className="close-search"
+              onClick={() => closePage(!show)}
+            >
+              Close
+            </a>
+            <div className="search-books-input-wrapper">
+              <input
+                type="text"
+                placeholder="Search by title, author, or ISBN"
+              />
+            </div>
+          </div>
+          <div className="search-books-results">
+            <ol className="books-grid"></ol>
+          </div>
+        </div>
+    )
+}
+
+export default Search;
